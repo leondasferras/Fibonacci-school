88123eb1eed2b4b3d78a326169e4347a303d360b
leondasferras <90497102+leondasferras@users.noreply.github.com>
2022-10-24 02:06:43 +0500

    queque-page fixes and add list-page styles

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+import {useState} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from './list-page.module.css'
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
 export const ListPage: React.FC = () => {
+
+  const DefaultArray = Array.from({ length: 7 }, (v, i) => ({
+    el: "",
+    state: ElementStates.Default,
+    isTail: false,
+    isHead: false,
+  }));
+
+
+  const [number, setNumber] = useState();
+  const [index, setIndex] = useState();
+  const [array, setArray] = useState([]);
+
+
+
+
+
+
   return (
     <SolutionLayout title="Связный список">
+      <div className={styles.buttonsWrapper}>   
+        <Input maxLength={4} isLimitText={true} getInputValue={setNumber} placeholder='Введите значение'/>
+        <Button text="Добавить в head"/>
+        <Button text="Добавить в tail"/>
+        <Button text="Удалить из head"/>
+        <Button text="Удалить из tail"/>
+        <Input max={9} type="number" getInputValue={setIndex} placeholder='Введите индекс'/>
+        <Button text="Добавить по индексу"/>
+        <Button text="Удалить по индексу"/>
 
+      </div>
+      <div className={styles.circlesWrapper}>
+        {DefaultArray.map((item) => 
+        <>
+        <Circle letter={item.el}/>
+
+        </>
+        )}
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -6,41 +6,92 @@ import { Circle } from "../ui/circle/circle";
 import { Button } from "../ui/button/button";
 import { ElementStates } from "../../types/element-states";
 import { timeout } from "../../utils/timeout";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const QueuePage: React.FC = () => {
-  const DefaultArray = Array.from({length:7}, (v,i) =>  ({el:'', state:ElementStates.Default, isTail:false, isHead:false})   )
+  const DefaultArray = Array.from({ length: 7 }, (v, i) => ({
+    el: "",
+    state: ElementStates.Default,
+    isTail: false,
+    isHead: false,
+  }));
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [queueArray, setQueueArray] = useState<
-    Array<{ el: number | string; state: ElementStates; isTail:boolean; isHead:boolean }>
+    Array<{
+      el: number | string;
+      state: ElementStates;
+      isTail: boolean;
+      isHead: boolean;
+    }>
   >(DefaultArray);
 
- 
+  const handleAddElement = async () => {
+    setIsLoading(true);
+    const tempArray = queueArray;
+    const n = tempArray.length;
 
-  const handleAddElement = async() => {
-    const tempArray = queueArray
-    const n = tempArray.length
-    const targetElement = tempArray.reverse().findIndex((item) => item.el !== '')
-    tempArray.reverse()
-    if (targetElement === -1) {tempArray[0].el = inputValue }
-    else {tempArray[n-targetElement].el = inputValue }
-    setQueueArray([...tempArray])
-  }
+    const moveTail = () => {
+      const currentTail = tempArray.findIndex((item) => item.isTail === true);
+      tempArray[currentTail].isTail = false;
+      if (tempArray[currentTail + 1].el)
+        tempArray[currentTail + 1].isTail = true;
+    };
 
+    const targetElement = tempArray
+      .reverse()
+      .findIndex((item) => item.el !== "");
+    tempArray.reverse();
+    if (targetElement === -1) {
+      tempArray[0].state = ElementStates.Changing;
+      tempArray[0].el = inputValue;
+      tempArray[0].isHead = true;
+      tempArray[0].isTail = true;
 
-  const handleDeleteElement = async() => {
-    const tempArray = queueArray
-    const targetElement = tempArray.findIndex((item) => item.el !== '')
-    tempArray[targetElement].el = ''
-    setQueueArray([...tempArray])
-  }
-  const handleResetQueue = async() => {
+      setQueueArray([...tempArray]);
+      await timeout(SHORT_DELAY_IN_MS);
+      tempArray[0].state = ElementStates.Default;
+    } else {
+      tempArray[n - targetElement].state = ElementStates.Changing;
+      tempArray[n - targetElement].el = inputValue;
+      moveTail();
+      setQueueArray([...tempArray]);
+      await timeout(SHORT_DELAY_IN_MS);
+      tempArray[n - targetElement].state = ElementStates.Default;
+    }
 
-  }
+    setQueueArray([...tempArray]);
+    setInputValue("");
+    setIsLoading(false);
+  };
 
-  
+  const handleDeleteElement = async () => {
+    setIsLoading(true);
+    const tempArray = queueArray;
+    const targetElement = tempArray.findIndex((item) => item.el !== "");
+    const moveHead = () => {
+      const currentHead = tempArray.findIndex((item) => item.isHead === true);
+      tempArray[currentHead].isHead = false;
+      if (tempArray[currentHead + 1]?.el)
+        tempArray[currentHead + 1].isHead = true;
+    };
 
+    tempArray[targetElement].state = ElementStates.Changing;
+    setQueueArray([...tempArray]);
+    await timeout(SHORT_DELAY_IN_MS);
 
+    tempArray[targetElement].el = "";
+    tempArray[targetElement].isTail = false;
+    tempArray[targetElement].state = ElementStates.Default;
+
+    moveHead();
+    setQueueArray([...tempArray]);
+    setIsLoading(false);
+  };
+
+  const handleResetQueue = async () => {
+    setQueueArray([...DefaultArray]);
+  };
 
   return (
     <SolutionLayout title="Очередь">
@@ -52,23 +103,28 @@ export const QueuePage: React.FC = () => {
             maxLength={4}
             getInputValue={setInputValue}
             value={inputValue}
+            disabled={isLoading}
           />
           <Button
             text="Добавить"
             onClick={handleAddElement}
-            disabled={!inputValue || isLoading ||  queueArray[queueArray.length-1].el !== ''}
+            disabled={
+              !inputValue ||
+              isLoading ||
+              queueArray[queueArray.length - 1].el !== ""
+            }
             isLoader={isLoading}
           />
           <Button
             text="Удалить"
             onClick={handleDeleteElement}
-            disabled={isLoading  }
+            disabled={isLoading || !queueArray.some((el) => el.el !== "")}
           />
         </div>
         <Button
           text="Очистить"
           onClick={handleResetQueue}
-          disabled={isLoading || queueArray.length < 1}
+          disabled={isLoading || !queueArray.some((el) => el.el !== "")}
         />
       </div>
 
@@ -81,7 +137,8 @@ export const QueuePage: React.FC = () => {
               state={el.state}
               key={i}
               index={i}
-              head={el.state === ElementStates.Changing ? "top" : ""}
+              head={el.isHead ? "Head" : ""}
+              tail={el.isTail ? "Tail" : ""}
             />
           );
         })}
